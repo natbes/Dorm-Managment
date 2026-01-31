5fb0888b1ff9b37ec3fd0c54f8c6528dd2c3ceb8
natbes <110822720+natbes@users.noreply.github.com>
2026-01-31 11:27:12 +0300

    Add User class with properties and methods
    Initialization of fields for the base user class

---
+++ src/main/java/dorm/model/User.java
@@ -0,0 +1,42 @@
+package dorm.model;
+
+import java.util.Objects;
+
+public class User {
+    private final String id;
+    private final String username;
+    private String password;
+    private final Role role;
+    private final String displayName;
+
+    public User(String id, String username, String password, Role role, String displayName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.displayName = displayName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
