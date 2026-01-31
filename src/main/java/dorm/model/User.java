c1b3ab2e3660225c047545527b1c30d0573cbb40
natbes <110822720+natbes@users.noreply.github.com>
2026-01-31 11:42:10 +0300

    Add equals() and hashCode()

--- src/main/java/dorm/model/User.java
+++ src/main/java/dorm/model/User.java
@@ -40,3 +40,22 @@ public class User {
     public String getDisplayName() {
         return displayName;
     }
+
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
