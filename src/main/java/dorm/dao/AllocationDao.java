a2e131d03867b8f94178d1eb8c1a1a2e58539edf
kuku58 <marikuku47@gmail.com>
2026-02-02 12:09:45 +0300

    Delete src/main/java/dorm/dao/AllocationDao.java

--- src/main/java/dorm/dao/AllocationDao.java
+++
@@ -1,6 +0,0 @@
-package dorm.dao;
-
-public interface AllocationDao {
-    void allocate(long applicationId, long bedId, long assignedByUserId, String roomNumber);
-    void freeBedIfAllocated(long applicationId);
-}
